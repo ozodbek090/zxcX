0f9f8be990060a987a582b6c0160226b1b92a498
ozodbek090 <itmars047@gmail.com>
2025-09-13 15:22:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const Uzerneme1 = (Math.floor(Math.random() * 22))
+alert(`Sizga xozir random son keladi 🤪`)
+alert(`Ramdom orqali chiqqan son:  ${Uzerneme1}`)
+confirm(`diz ramdom soni 2 ga kopaytiriladi va bo'lmoqchimiz !`)
+
+const Uzervem1 = (`${Uzerneme1}` * 2 );
+const Uzervem2 = (`${Uzerneme1}` / 2 );
+const Uzervem3 = (`${Uzerneme1}` % 3 );
+
+alert (`
+    Ramdom orqali chiqqan son: ${Uzerneme1}
+    diz ramdom soni 2 ga kopaytiriladi ${Uzervem1} son chqardi:
+    va shu sonii 2 ga bo'lib ${Uzervem2} son chqardi:
+    va shu sonii 3 ga bolgandagi qoldiq: ${Uzervem3} sonini chupardik 
+
+    `)
